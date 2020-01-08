8357279d0a9636eb9da9d59c2f028c96759aca24
takashi.yamaguchi <takashi.yamaguchi@techacademy.jp>
2020-01-08 08:33:42 +0000

    add statuscolumn

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
+    <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
     <div class="row">
         <div class="col-6">
@@ -11,6 +11,11 @@
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
         
                 {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
